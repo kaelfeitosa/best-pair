0dbad9a097d6ad1444ce3214f2c18c1ba2d2ad15
Narciso Benigno <nbenigno@thoughtworks.com>
2015-08-01 11:45:06 -0300

    Fix latest pairs issue

--- src/main/scala/pairiator/service/LatestPairs.scala
+++ src/main/scala/pairiator/service/LatestPairs.scala
@@ -15,22 +15,23 @@ import org.joda.time.ReadableInstant
   import pairiator.service._
   import pairiator.model._
   import pairiator.repository._
-  import org.joda.time.DateTime
+  import org.joda.time._
   
   val ps = LatestPairings(new ProjectRepository, new CommitRepository)
-  ps.pairings((new DateTime()).minusDays(2))
+  ps.pairings(new LocalDate().minusDays(2))
  */
 
 case class LatestPairings(projects: ProjectRepository, commits: CommitRepository) {
-  implicit val sortByCreatedAt = Ordering.by[DateTime, Int](_.millisOfDay().get) 
+  implicit val sortByCreatedAt = Ordering.by[DateTime, Long](_.getMillis()).reversed()
   
   def pairings(since: LocalDate): List[Pairing] = {
     projects.list()
       .takeWhile(_.lastActivityAt.isAfter(since.toDateTimeAtStartOfDay()))
       .flatMap(commits.listBy(_))
+      .sortBy(_.createdAt)
       .takeWhile(_.createdAt.isAfter(since.toDateTimeAtStartOfDay()))
       .map { commit => (commit.createdAt.toLocalDate(), Pairing.by(commit)) }
-      .groupBy { pairing => pairing._1 }
+      .groupBy(_._1)
       .map(_._2.toSet.toList).flatten.map(_._2)
       .groupBy(_.pair).values
       .map { pairings => Pairing(pairings.head.pair, pairings.size) }.toList
