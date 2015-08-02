37b41287e6be4f761242e88682d867549736a850
Narciso Benigno <nbenigno@thoughtworks.com>
2015-08-01 22:51:34 -0300

    Improve the pairing representation

--- src/main/scala/pairiator/controllers/PairingsController.scala
+++ src/main/scala/pairiator/controllers/PairingsController.scala
@@ -2,31 +2,28 @@ package pairiator.controllers
 
 import org.joda.time.DateTime
 import org.joda.time.LocalDate
-
 import com.twitter.finagle.http._
 import com.twitter.finatra.http._
-
 import pairiator.model._
 import pairiator.repository._
 import pairiator.service.LatestPairings
-
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
+import pairiator.model.Commiter
 
 class PairingsController extends Controller {
   implicit val jodaDateTimeWrites = Writes.jodaDateWrites("yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
   
-  implicit val commitWrites: Writes[Commiter] =
-    (__ \ "author_email").write[String].contramap(_.name)
-  
-  implicit def tuple2Writes[A, B](implicit a: Writes[A], b: Writes[B]): Writes[Tuple2[A, B]] =
-    new Writes[Tuple2[A, B]] {
-      def writes(tuple: Tuple2[A, B]) =
-        JsArray(Seq(a.writes(tuple._1), b.writes(tuple._2)))
+  implicit def tuple2Writes = new Writes[Tuple2[Commiter, Commiter]] {
+      def writes(pairs: Tuple2[Commiter, Commiter]) =
+        Json.obj(
+            "pilot" -> pairs._1.name,
+            "navigarot" -> pairs._2.name
+        )
     }
   
   implicit val pairingsWrites: Writes[Pairing] = (
-    (__ \ "pair").write[(Commiter, Commiter)] and
+    __.write[(Commiter, Commiter)] and
     (__ \ "pairings").write[Int]
   )(unlift(Pairing.unapply))
 
@@ -34,7 +31,11 @@ class PairingsController extends Controller {
     val latests = LatestPairings(new ProjectRepository, new CommitRepository)
     
     
-    val json = Json.toJson(latests.pairings(LocalDate.now().minusDays(2)))
+    val json = Json.obj(
+        "_embedded" -> Json.obj(
+            "pairing" -> Json.toJson(latests.pairings(LocalDate.now().minusDays(2)))
+        )
+    )
     
     response.ok(Json.stringify(json)).contentType("application/hal+json")
   }
